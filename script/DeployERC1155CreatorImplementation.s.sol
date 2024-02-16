30ea63852ba50b3fdca6b1b774faabb8f2c823da
Abdul Qawi Bhoira <aqawi72@gmail.com>
2024-02-16 22:03:00 +0400

    deploy changes

--- script/DeployERC1155CreatorImplementation.s.sol
+++ script/DeployERC1155CreatorImplementation.s.sol
@@ -8,7 +8,7 @@ contract DeployERC1155CreatorImplementation is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        new ERC1155CreatorImplementation{salt: 0x4552433131353543726561746f72496d706c656d656e746174696f6e45524331}();
+        new ERC1155CreatorImplementation();
         vm.stopBroadcast();
     }
 }
